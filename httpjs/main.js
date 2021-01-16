7148a7b1656d66bd8375d3653fb8480cde62a668
xuhao <642508924@qq.com>
2021-01-16 21:46:13 +0800

    1

--- httpjs/main.js
+++ httpjs/main.js
@@ -1,16 +1,44 @@
 const http = require('http')
 
 
-http.get("http://c.y.qq.com/soso/fcgi-bin/client_search_cp?w=少年", resp => {
-  let data = "";
-  resp.on("data", function (chunk) {
-    data += chunk;
-  });
-  resp.on("end", () => {
-    let jsonData = JSON.parse(data.match(/^callback\((.*)\)$/)[1])
-    console.log('数据==>', jsonData);
-  });
-  resp.on("error", err => {
-    console.log(err.message);
-  });
-});
+// http.get("http://c.y.qq.com/soso/fcgi-bin/client_search_cp?w=少年", resp => {
+//   let data = "";
+//   resp.on("data", function (chunk) {
+//     data += chunk;
+//   });
+//   resp.on("end", () => {
+//     let jsonData = JSON.parse(data.match(/^callback\((.*)\)$/)[1])
+//     console.log('数据==>', jsonData);
+//   });
+//   resp.on("error", err => {
+//     console.log(err.message);
+//   });
+// });
+
+
+// http.get("https://u.y.qq.com/cgi-bin/musicu.fcg", resp => {
+//   let data = "";
+//   resp.on("data", function (chunk) {
+//     data += chunk;
+//   });
+//   resp.on("end", () => {
+//     let jsonData = JSON.parse(data.match(/^callback\((.*)\)$/)[1])
+//     console.log('数据==>', jsonData);
+//   });
+//   resp.on("error", err => {
+//     console.log(err.message);
+//   });
+// });
+
+
+let aa = '/music/musicDownload/C400000S7TGL43hhBO.m4a'
+console.log(aa.split('/'))
+
+
+
+
+
+
+
+
+
